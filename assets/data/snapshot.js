dedb9ca032be6db3166bd015a05fe3a036c6fb67
Zavalon <hoshino.nzm@gmail.com>
2026-09-18 13:44:42 +0800

    ???:???? + ??????(????)

---
+++ assets/data/snapshot.js
@@ -0,0 +1,78 @@
+﻿/* =============================================================================
+ *  GitHub 离线快照（由 deploy-api.ps1 自动生成，一般不需要手动改）
+ *  用途：访客浏览器访问不了 api.github.com 时，页面用这份数据兜底。
+ *  生成时间：2026-09-18T05:43:20Z
+ * ========================================================================== */
+
+window.GITHUB_SNAPSHOT = {
+    "fetchedAt":  "2026-09-18T05:43:20Z",
+    "user":  {
+                 "login":  "Zava1on",
+                 "name":  "Zavalon",
+                 "avatar_url":  "https://avatars.githubusercontent.com/u/271201428?v=4",
+                 "html_url":  "https://github.com/Zava1on",
+                 "bio":  null,
+                 "blog":  "",
+                 "location":  null,
+                 "company":  null,
+                 "public_repos":  2,
+                 "public_gists":  0,
+                 "followers":  0,
+                 "following":  1,
+                 "created_at":  "2026-03-26T09:30:19Z"
+             },
+    "repos":  [
+                  {
+                      "name":  "Zava1on.github.io",
+                      "full_name":  "Zava1on/Zava1on.github.io",
+                      "html_url":  "https://github.com/Zava1on/Zava1on.github.io",
+                      "description":  "Zava1on ?????(GitHub Pages)",
+                      "homepage":  null,
+                      "language":  "HTML",
+                      "stargazers_count":  0,
+                      "forks_count":  0,
+                      "watchers_count":  0,
+                      "open_issues_count":  0,
+                      "fork":  false,
+                      "archived":  false,
+                      "size":  189,
+                      "topics":  [
+
+                                 ],
+                      "license":  null,
+                      "created_at":  "2026-09-18T04:30:22Z",
+                      "updated_at":  "2026-09-18T05:37:48Z",
+                      "pushed_at":  "2026-09-18T05:37:45Z",
+                      "default_branch":  "main"
+                  },
+                  {
+                      "name":  "TrivialGroup",
+                      "full_name":  "Zava1on/TrivialGroup",
+                      "html_url":  "https://github.com/Zava1on/TrivialGroup",
+                      "description":  "Just a milestone4TG",
+                      "homepage":  null,
+                      "language":  null,
+                      "stargazers_count":  0,
+                      "forks_count":  0,
+                      "watchers_count":  0,
+                      "open_issues_count":  0,
+                      "fork":  false,
+                      "archived":  false,
+                      "size":  14,
+                      "topics":  [
+
+                                 ],
+                      "license":  {
+                                      "key":  "gpl-3.0",
+                                      "name":  "GNU General Public License v3.0",
+                                      "spdx_id":  "GPL-3.0",
+                                      "url":  "https://api.github.com/licenses/gpl-3.0",
+                                      "node_id":  "MDc6TGljZW5zZTk="
+                                  },
+                      "created_at":  "2026-07-09T07:31:31Z",
+                      "updated_at":  "2026-07-09T07:31:44Z",
+                      "pushed_at":  "2026-07-09T07:31:32Z",
+                      "default_branch":  "main"
+                  }
+              ]
+};
